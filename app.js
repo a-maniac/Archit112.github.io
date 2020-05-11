39d79a0233a8176f0c1ca8a5e9e1b74e2607d1fd
a-maniac <58400326+a-maniac@users.noreply.github.com>
2020-05-11 12:21:35 +0530

    Added api
    Check for the API in the cmd

--- app.js
+++ app.js
@@ -7,7 +7,21 @@ app.use(express.static("public"))
 app.get('/',function(req,res){
   res.sendFile(__dirname+"/index.html")
 })
+var unirest = require("unirest");
 
+var req = unirest("GET", "https://covid19india.p.rapidapi.com/getIndiaStateData");
+
+req.headers({
+	"x-rapidapi-host": "covid19india.p.rapidapi.com",
+	"x-rapidapi-key": "0ed0d8c1bdmsh0ac4e941e21bd7cp1250c3jsnd79c0aff37ee"
+});
+
+
+req.end(function (res) {
+	if (res.error) throw new Error(res.error);
+
+	console.log(res.body);
+});
 
 
 app.listen(3000, function(){
